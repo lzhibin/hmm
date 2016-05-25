f1530975f64e5c759227df7d9532e15603a11408
lzhibin <342640194@qq.com>
2016-05-25 15:03:39 +0800

    missing data ,this version seems still have some bugs

--- hmm_missingdata.R
+++ hmm_missingdata.R
@@ -55,10 +55,256 @@ HMM.data.missing=function(Y,S,beta_r,na=NA){
   return(list(Y.missing=Y.missing,R=R))
 }
 
+
+#2 MCMC
+mcmc.hmm=function(Y,R,X,b0,B0,c0,C0,E0,Et,S,m,beta0_r=NULL,lambda_y=1,lambda_r=1,rep=1){
+  #initial value
+  d=ncol(X)    #numbers of beta
+  p=length(E0) #levels of hidden variable 
+  Ti=ncol(Y)   #numbers of periods
+  NY=nrow(Y)   #numbers of samples
+  B0.inv=solve(B0)
+  if(is.null(beta0_r))
+    beta0_r=array(0,dim=c(p,2))
+  
+  #variables for every step
+  Bk=array(dim=c(d,d,p,Ti))
+  bk=array(dim=c(p,d,Ti))
+  ck=array(dim=c(p,Ti))
+  Ck=array(dim=c(p,Ti))
+  
+  betak=array(dim=c(p,d,Ti))
+  sigmak=array(dim=c(p,Ti))
+  betak_r=beta0_r
+  NS=array(dim=c(p,Ti))
+  NSt=array(dim=c(p,p,Ti-1))
+  Dt=array(dim=c(p,p,Ti-1)) #transfer matrix [f(u_j|u_hat_i)]
+  
+  #variables to save parameters
+  p.betak=array(dim=c(p,d,Ti,m))
+  p.sigmak=array(dim=c(p,Ti,m))
+  p.D=array(dim=c(p,m))
+  p.Dt=array(dim=c(p,p,Ti-1,m))
+  p.betak_r=array(dim=c(p,2,m))
+  
+  #init betak and sigmak for hmm
+  temp.beta=mvrnorm(p,b0,B0)
+  temp.sigma=rinvgamma(p,c0,scale=C0)
+  for(t in 1:Ti){
+    betak[,,t]=temp.beta
+    sigmak[,t]=temp.sigma
+  }
+  
+  #init missing Y base on prior distribution
+  for(t in 1:Ti)
+    for(ny in 1:NY){
+      if(is.na(Y[ny,t]))
+        Y[ny,t]=rnorm(1,X[ny,]%*%betak[S[ny,t],,t])
+    }
+  
+  #density function for FFBS to renew S
+  f.density=function(y,x,beta,sigma){
+    m=as.vector(beta%*%x)
+    s=sqrt(sigma)
+    res=dnorm(y,m,s)
+    return(res)
+  }
+  
+  #likelihood ratio for beta_r_new / beta_r_old
+  ln_beta_r_ratio=function(r,y,beta1,beta0){
+    #beta1 and beta0 are the new value and old value of beta_r
+    temp1=sum(r)*(beta1[1]-beta0[1])+sum(r*y)*(beta1[2]-beta0[2])
+    temp2=sum(log(1+exp(beta0[1]+beta0[2]*y)))-sum(log(1+exp(beta1[1]+beta1[2]*y)))
+    temp=exp(temp1+temp2)
+    return(temp)
+    #exp to diff of log likelihood
+  }
+  
+  #information matrix estimator for beta_r ;return inverse
+  im_beta_r=function(y,beta=NULL){
+    #the beta here should be the estimator of beta_r ;using zero vector as default
+    temp=array(dim=c(2,2))
+    if(is.null(beta)){
+      temp[1,1]=length(y)
+      temp[1,2]=sum(y)
+      temp[2,1]=temp[1,2]
+      temp[2,2]=sum(y^2)
+      #it means beta=c(0,0)
+    }
+    else{
+      d1=exp(beta[1]+beta[2]*y)
+      d=d1/((1+d1)^2)
+      temp[1,1]=sum(d)
+      temp[1,2]=sum(y*d)
+      temp[2,1]=temp[1,2]
+      temp[2,2]=sum(d*(y^2))
+    }
+    return(solve(temp))
+  }
+  
+  #likelihood ratio for yi_new / yi_old
+  ln_yi_ratio=function(beta,x,sigma,y1,y0){
+    #the beta here is betak[k,,t];sigma is sigmak[k,t] y1 and y0 is the new value and old value
+    temp1=((y0-sum(x*beta))^2-(y1-sum(x*beta))^2)/(2*sigma) #+beta[2]*r*(y1-y0)=0 for r=0
+    temp2=log(1+exp(beta[1]+beta[2]*y0))-log(1+exp(beta[1]+beta[2]*y1))
+    temp=exp(temp1+temp2)
+    return(temp)
+    #take exp to diff of log likelihood
+  }
+  
+  #information matrix estimator for yi ;return inverse ;in this sample it is number
+  im_yi=function(beta,sigma,y=0){
+    d1=(beta[2]^2)
+    d2=exp(beta[1]+beta[2]*y)
+    temp=(d1*d2)/((1+d2)^2)+1/sigma
+    return(1/temp)
+  }
+  
+  
+  #main iteration
+  for(i in 1:m){
+    for(t in 1:Ti){
+      NS[,t]=sapply(1:p,function(x)sum(S[,t]==x))
+    }
+    D=rdirichlet(1,E0+NS[,1])
+    #generate init distribution
+    
+    for(t in 1:(Ti-1)){
+      for(k in 1:p){
+        NSt[k,,t]=sapply(1:p,function(x)sum(S[S[,t]==k,t+1]==x))
+        Dt[k,,t]=rdirichlet(1,Et[k,,t]+NSt[k,,t])
+      }
+    }
+    #generate transfer matrix
+    
+    for(t in 1:Ti){
+      for(j in 1:p){
+        Bk[,,j,t]=solve(B0.inv+t(X[S[,t]==j,])%*%X[S[,t]==j,]/sigmak[j,t])
+        bk[j,,t]=Bk[,,j,t]%*%(B0.inv%*%b0+t(X[S[,t]==j,])%*%Y[S[,t]==j,t]/sigmak[j,t])
+        
+        betak[j,,t]=mvrnorm(1,bk[j,,t],Bk[,,j,t])
+        
+        ck[j,t]=c0+NS[j,t]/2
+        Ck[j,t]=C0+sum((Y[S[,t]==j,t]-X[S[,t]==j,]%*%betak[j,,t])^2)/2
+        
+        sigmak[j,t]=rinvgamma(1,ck[j,t],scale=Ck[j,t])
+        
+      }
+    }
+    
+    
+    #FFBS renew hidden variable altnative code
+    P.S.Update=array(dim=c(Ti,p,NY))
+    for(nr in 1:NY){
+      P.S.Update[1,,nr]=D*f.density(Y[nr,1],X[nr,],betak[,,1],sigmak[,1])
+      for(t in 2:Ti){
+        P.S.Update[t,,nr]=t(P.S.Update[t-1,,nr])%*%Dt[,,t-1]*f.density(Y[nr,t],X[nr,],betak[,,t],sigmak[,t])
+      }
+    }
+    for(nr in 1:NY){
+      S[nr,Ti]=sample.int(p,1,prob = P.S.Update[Ti,,nr])
+      for(t in (Ti-1):1){
+        s.temp=P.S.Update[t,,nr]*Dt[,S[nr,t+1],t]
+        S[nr,t]=sample.int(p,1,prob = s.temp)
+      }
+    }
+    
+    #sample beta_r 
+    for(k in 1:p){
+      yk=Y[S==k]
+      rk=R[S==k]
+      temp=mvrnorm(1,betak_r[k,],lambda_r*im_beta_r(yk))  #lambda_r to control accept rate
+      alpha=min(1,ln_beta_r_ratio(rk,yk,temp,betak_r[k,]))
+      cat("acceptrate_beta:",alpha,"\n")  #to see the accept rate
+      U01=runif(1)
+      if(U01<alpha)
+        betak_r[k,]=temp
+    }
+    
+    #sample missing yi
+    for(ny in 1:NY)
+      for(t in 1:Ti)
+        if(R[ny,t]==0){
+          nyt.s=S[ny,t]
+          temp=rnorm(1,Y[ny,t],sqrt(lambda_y*im_yi(betak_r[nyt.s,],sigmak[nyt.s,t])))
+          alpha=min(1,ln_yi_ratio(beta=betak[nyt.s,,t],x=X[ny,],sigma=sigmak[nyt.s,t],y1=temp,y0=Y[ny,t]))
+          cat("acceptrate_yi:",alpha,"\n")  #to see the accept rate
+          U01=runif(1)
+          if(U01<alpha)
+            Y[ny,t]=temp
+        }
+    
+    
+    #label switch
+    base=betak[,1,]
+    for(t in 1:Ti){
+      betak[,,t]=betak[order(base[,t]),,t]
+      sigmak[,t]=sigmak[order(base[,t]),t]
+      S[,t]=rank(base[,t])[S[,t]]
+    }
+    
+    betak_r=betak_r[order(base[,1]),]
+    D=D[order(base[,1])]
+    
+    for(t in 1:(Ti-1)){
+      #switch Dt
+      Dt[,,t]=Dt[order(base[,t]),order(base[,t+1]),t]
+      
+    }
+    
+    #store parameters
+    p.betak[,,,i]=betak
+    p.sigmak[,,i]=sigmak
+    p.D[,i]=D
+    p.Dt[,,,i]=Dt
+    p.betak_r[,,i]=betak_r
+  }
+  ParaMeters=list(p.betak=p.betak,p.sigmak=p.sigmak,p.D=p.D,p.Dt=p.Dt,p.betak_r=p.betak_r)
+  class(ParaMeters)="hmm.missing"
+  if(rep==1)
+    return(ParaMeters)
+  else{
+    groups=list()
+    groups[[1]]=ParaMeters
+    for(r in 2:rep)
+      groups[[r]]=mcmc.hmm(Y,R,X,b0,B0,c0,C0,E0,Et,S,m,beta0_r,lambda_y,lambda_r,rep=1)
+    class(groups)='hmmgroups.missing'
+  }
+  return(groups)
+}
+
+#method to summary hmm.missing class
+summary.hmm.missing=function(data,n){
+  len=dim(data$p.D)[2]
+  if(len<n){
+    print("error! need more data")
+  }
+  
+  beta=apply(data$p.betak[,,,(len-n+1):len],c(1,2,3),mean)
+  beta.sd=apply(data$p.betak[,,,(len-n+1):len],c(1,2,3),sd)
+  sigma=apply(data$p.sigmak[,,(len-n+1):len],c(1,2),mean)
+  sigma.sd=apply(data$p.sigmak[,,(len-n+1):len],c(1,2),sd)
+  init.distribution=apply(data$p.D[,(len-n+1):len],1,mean)
+  init.distribution.sd=apply(data$p.D[,(len-n+1):len],1,sd)
+  transfer.matrix=apply(data$p.Dt[,,,(len-n+1):len],c(1,2,3),mean)
+  transfer.matrix.sd=apply(data$p.Dt[,,,(len-n+1):len],c(1,2,3),sd)
+  beta_r=apply(data$p.betak_r[,,(len-n+1):len],c(1,2),mean)
+  beta_r.sd=apply(data$p.betak_r[,,(len-n+1):len],c(1,2),sd)
+  
+  summary.hmm=list(beta=beta,beta.sd=beta.sd,sigma=sigma,sigma.sd=sigma.sd,
+                   init.distribution=init.distribution,
+                   init.distribution.sd=init.distribution.sd,
+                   transfer.matrix=transfer.matrix,
+                   transfer.matrix.sd=transfer.matrix.sd,
+                   beta_r=beta_r,
+                   beta_r.sd=beta_r.sd)
+  return(summary.hmm)
+}
+
 #test parameter to generate missing data
-sz=700  #sample size
-X=matrix(c(rep(1,sz),runif(sz,0,5)),sz,2)
-beta=rbind(c(-1,0.5),c(1,-0.5))
+sz=1000  #sample size
+X=matrix(c(rep(1,sz),runif(sz,-5,5)),sz,2)
+beta=rbind(c(-2,1),c(2,-1))
 beta.t=rep(beta,3)
 dim(beta.t)=c(2,2,3)
 sigma=c(0.3,0.3)
@@ -71,9 +317,19 @@ dim(Prob.t)=c(2,2,2)
 temp=HMM.data(X,Prob,Prob.t,beta.t,sigma.t,T)
 Y=temp$Y
 S=temp$S
-beta_r=array(c(2,2,-1.5,-1.5),dim=c(2,2))
+beta_r=array(c(2,2,-3,-3),dim=c(2,2))
 temp.missing=HMM.data.missing(Y,S,beta_r)
 Y.missing=temp.missing$Y.missing
 R=temp.missing$R
 table(R)
 R[Y > 2]
+
+b0=solve(t(X)%*%X)%*%t(X)%*%Y[,1]
+B0=1.5*diag(2)
+c0=1.28
+C0=0.36*var(as.vector(Y))
+E0=c(1,1)
+Et=Prob.t*2
+m=2000
+system.time(g<-mcmc.hmm(Y,R,X,b0,B0,c0,C0,E0,Et,S,m,beta_r,lambda_r = 5,lambda_y = 5))
+summary(g,m/2)
